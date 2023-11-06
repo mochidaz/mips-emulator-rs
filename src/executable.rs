d8e1b2e8268e8d418dba485049675cbf5a2c67d5
Muhammad Gusti Nurfathin (Native-M) <gustipart2@gmail.com>
2023-11-07 00:54:37 +0700

    add instruction and executable

---
+++ src/executable.rs
@@ -0,0 +1,16 @@
+use instr::Instruction;
+
+pub struct Executable {
+	code: Vec<u32>,
+	start_pc: u32
+}
+
+impl Executable {
+	pub fn assemble(instructions: Vec<Instruction>) {
+		for i in instructions {
+			match i {
+
+			}
+		}
+	}
+}
---
+++ src/instr.rs
@@ -0,0 +1,14 @@
+use register::RegID;
+
+pub enum Instruction {
+	Add(RegID, RegID, RegID),
+	Sub(RegID, RegID, RegID),
+	Addi(RegID, RegID, i32),
+	Addu(RegID, RegID, RegID),
+	Subu(RegID, RegID, RegID),
+	Mul(RegID, RegID, RegID),
+	Mult(RegID, RegID),
+	Div(RegID, RegID),
+	Add(),
+	Add(),
+}
