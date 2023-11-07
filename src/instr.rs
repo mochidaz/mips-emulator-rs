77207abdda28a0ecb3081db813c9e683cc03223e
Muhammad Gusti Nurfathin (Native-M) <gustipart2@gmail.com>
2023-11-07 20:03:56 +0700

    Simplify instruction encoding

--- src/instr.rs
+++ src/instr.rs
@@ -2,12 +2,18 @@ use crate::register::RegID;
 
 pub enum AsmInstr {
 	Add(RegID, RegID, RegID),
+    Addu(RegID, RegID, RegID),
+    Addi(RegID, RegID, i32),
 	Sub(RegID, RegID, RegID),
-	Addi(RegID, RegID, i32),
-	Addu(RegID, RegID, RegID),
-	Subu(RegID, RegID, RegID),
+    Subu(RegID, RegID, RegID),
 	Mul(RegID, RegID, RegID),
 	Mult(RegID, RegID),
 	Div(RegID, RegID),
 }
 
+pub enum RtypeFn {
+    Add = 0b100000,
+    Addu = 0b100001,
+    Sub = 0b100010,
+    Subu = 0b100011,
+}
