d21b148c3323397876fe45054d81d8e1c1df3164
Rahman Hakim <rahmanhakim2435@gmail.com>
2023-11-07 20:21:26 +0700

    add more opcodes

--- src/instr.rs
+++ src/instr.rs
@@ -28,6 +28,21 @@ pub enum AsmInstr {
 pub enum RtypeFn {
     Add = 0b100000,
     Addu = 0b100001,
+	Addi = 0b001000,
     Sub = 0b100010,
     Subu = 0b100011,
+	Mul = 0b000010,
+	Mult = 0b011000,
+	Div = 0b011010,
+
+	And = 0b100100,
+	Andi = 0b001100,
+	Or = 0b100101,
+	Ori = 0b001101,
+}
+
+pub enum OpCode {
+	Lw = 0b100011,
+	Sw = 0b101011,
+	Lui = 0b001111,
 }
