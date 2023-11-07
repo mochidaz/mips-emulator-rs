24221b7d6b7a16d15b4b3ded42d89de32375d241
Rahman Hakim <rahmanhakim2435@gmail.com>
2023-11-07 11:12:39 +0700

    add more instructions

--- src/instr.rs
+++ src/instr.rs
@@ -1,6 +1,7 @@
 use crate::register::RegID;
 
 pub enum Instruction {
+	// Arithmetic
 	Add(RegID, RegID, RegID),
 	Sub(RegID, RegID, RegID),
 	Addi(RegID, RegID, i32),
@@ -9,4 +10,17 @@ pub enum Instruction {
 	Mul(RegID, RegID, RegID),
 	Mult(RegID, RegID),
 	Div(RegID, RegID),
+
+	// Logical
+	And(RegID, RegID, RegID),
+	Andi(RegID, RegID, i32),
+	Or(RegID, RegID, RegID),
+	Ori(RegID, RegID, i32),
+	Sll(RegID, RegID, i32),
+	Srl(RegID, RegID, i32),
+
+	// Data Transfer
+	Lw(RegID, i32, RegID),
+	Sw(RegID, i32, RegID),
+	Lui(RegID, i32),
 }
